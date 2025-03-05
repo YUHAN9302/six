58a6f82277c719c90cd2c0b75b4019d339f01bd6
cocofruit0423 <nicole66334@gmail.com>
2025-03-06 02:13:38 +0800

    開始介面/主房間介面

---
+++ Assets/COCO/暫停介面/暫停.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class 暫停 : MonoBehaviour
+{
+    public GameObject Quit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+    public void GameQuit()
+    {
+        Application.Quit();
+    }
+    public void second()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
