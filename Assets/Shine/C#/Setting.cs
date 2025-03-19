3eabc428bf49befc51dd0fe974feb3f5f1490f98
Shineliu77 <33471613+Shineliu77@users.noreply.github.com>
2025-03-19 23:41:12 +0800

    設定介面參數撰寫-畫質尚未寫

--- Assets/Shine/C#/Setting.cs
+++ Assets/Shine/C#/Setting.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using TMPro;
 public class Setting : MonoBehaviour
 {
+    public Slider BGMSlider;
+    public Slider SFXSlider;
+    public AudioMixer AudioMixerObj;
+    public TMP_Dropdown ScreenDropdown;
     // Start is called before the first frame update
     void Awake()
     {
@@ -15,5 +21,23 @@ public class Setting : MonoBehaviour
     void Update()
     {
         
+    }
+    public void ChangeBGMSlider()
+    {
+        AudioMixerObj.SetFloat("BGMSound", BGMSlider.value);
+    }
+    public void ChangeSFXSlider()
+    {
+        AudioMixerObj.SetFloat("SFXSound", SFXSlider.value);
+    }
+    public void ChangeScreenSize() {
+        switch (ScreenDropdown.value) {
+            case 0:
+                Screen.SetResolution(1920, 1080, false);
+                break;
+            case 1:
+                Screen.SetResolution(800, 480, false);
+                break;
+        }
     }
 }
