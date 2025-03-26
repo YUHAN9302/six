1e63b75d0c2b89c18ec8dbde7fa670c4bb314cca
Snowfox-22 <ykkitsune06152@gmail.com>
2025-03-26 23:11:56 +0800

    燈光打好

--- Assets/Shine/C#/ClickCandleOpenSavePage.cs
+++ Assets/Shine/C#/ClickCandleOpenSavePage.cs
@@ -1,17 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class ClickCandleOpenSavePage : MonoBehaviour
 {
     public GameObject SavePage;
+    public GameObject AniUI2;
     public GameObject AniUI;
     public AnimationClip anim;
     public GameObject PlayerAniUI;
+    public Light2D[] candleLights;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        AniUI2.SetActive(true);
+        foreach (Light2D light in candleLights)
+        {
+            if (light != null)
+            {
+                light.enabled = false;
+            }
+        }
     }
 
     // Update is called once per frame
@@ -25,7 +36,15 @@ public class ClickCandleOpenSavePage : MonoBehaviour
     }
     private void OnMouseDown()
     {
+        AniUI2.SetActive(false);
         AniUI.SetActive(true);
+        foreach (Light2D light in candleLights)
+        {
+            if (light != null)
+            {
+                light.enabled = true;
+            }
+        }        
         StartCoroutine(Wait());
     }
     IEnumerator Wait() {
