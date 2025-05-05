1d2050542669029a24a9fed5127b22b90bca4591
cocofruit0423 <nicole66334@gmail.com>
2025-05-06 02:34:43 +0800

    物品箱開關ok

---
+++ Assets/COCO/coco的程式碼/BOX.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BOX : MonoBehaviour
+{
+    public Animator boxAnimator;
+    public Button boxButton;
+
+    private bool isBoxOpen = false;
+    private bool isAnimating = false;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        boxButton.onClick.AddListener(OnBoxButtonClick);
+    }
+
+    void OnBoxButtonClick()
+    {
+        if (isAnimating) return; // 防止動畫中重複觸發
+
+        Debug.Log("按到了！");
+
+        if (!isBoxOpen)
+        {
+            boxAnimator.ResetTrigger("CloseBox");
+            boxAnimator.SetTrigger("OpenBox");
+            StartCoroutine(WaitForAnimation("OpenBox"));
+        }
+        else
+        {
+            boxAnimator.ResetTrigger("OpenBox");
+            boxAnimator.SetTrigger("CloseBox");
+            StartCoroutine(WaitForAnimation("CloseBox"));
+        }
+
+        isBoxOpen = !isBoxOpen;
+    }
+
+    private System.Collections.IEnumerator WaitForAnimation(string stateName)
+    {
+        isAnimating = true;
+
+        AnimatorStateInfo stateInfo = boxAnimator.GetCurrentAnimatorStateInfo(0);
+        while (!stateInfo.IsName(stateName))
+        {
+            yield return null;
+            stateInfo = boxAnimator.GetCurrentAnimatorStateInfo(0);
+        }
+
+        yield return new WaitForSeconds(stateInfo.length);
+        isAnimating = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
