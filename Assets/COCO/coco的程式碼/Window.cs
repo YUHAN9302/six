0658e383992ea4b92e666bd02aa5814a8fb2570c
cocofruit0423 <nicole66334@gmail.com>
2025-05-05 23:14:38 +0800

    修改BGM和台詞

--- Assets/COCO/coco的程式碼/Window.cs
+++ Assets/COCO/coco的程式碼/Window.cs
@@ -6,6 +6,8 @@ public class Window : MonoBehaviour
 {
     public GameObject windowAnimation;  // 書本動畫的 GameObject
     private bool animationPlayed = false;  // 動畫是否播放過
+    public GameObject windowSoundObject;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,11 +23,32 @@ public class Window : MonoBehaviour
         {
             windowAnimation.SetActive(true);  // 顯示書本動畫
 
+            if (windowSoundObject != null)
+            {
+                StartCoroutine(PlaySoundAndHide(windowSoundObject));
+            }
+
             Destroy(this);
         }
 
     }
-   
+
+    IEnumerator PlaySoundAndHide(GameObject soundObject)
+    {
+        soundObject.SetActive(true);
+        AudioSource audio = soundObject.GetComponent<AudioSource>();
+
+        if (audio != null)
+        {
+            audio.Play();
+
+            // 等待音效播放完成
+            yield return new WaitForSeconds(audio.clip.length);
+        }
+
+        soundObject.SetActive(false); // 播放完畢後隱藏
+    }
+
     // Update is called once per frame
     void Update()
     {
